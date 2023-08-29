4a5669130a477bb6a162a6e0f04a52a8a53ae019
Michele Bianco <bianco.michele@outlook.com>
2023-08-29 16:16:13 +0200

    Aggiunto space tag a immagini

--- backend/sql.py
+++ backend/sql.py
@@ -27,10 +27,10 @@ def executeQuery(query):
     
     return results
 
-def selectNCollections(latitudine, longitudine, n):
+def selectNCollections(longitudine, latitudine, n):
     # Query spaziale delle n collezioni più viine
     query = f"""
-        SELECT id, name, ST_Y(geom), ST_X(geom), ST_Distance(geom, ST_SetSRID(ST_MakePoint({latitudine}, {longitudine}), 0)) AS distance
+        SELECT id, name, ST_X(geom), ST_Y(geom), ST_Distance(geom, ST_SetSRID(ST_MakePoint({longitudine}, {latitudine}), 0)) AS distance
         FROM collections
         ORDER BY distance
         LIMIT {n}
@@ -38,7 +38,7 @@ def selectNCollections(latitudine, longitudine, n):
     results = executeQuery(query)
     
     # Converti la lista di tuple in una lista di dizionari
-    data_dict = [dict(zip(['id', 'nome', 'latitudine', 'longitudine', 'distanza'], item)) for item in results]
+    data_dict = [dict(zip(['id', 'nome', 'longitudine', 'latitudine', 'distanza'], item)) for item in results]
 
     # Formatta la lista di dizionari in JSON
     data_json = json.dumps(data_dict, indent=4)
@@ -51,15 +51,17 @@ def insertCollection():
     longitude = 11.231231
 
     # Query per l'inserimento di una nuova collection
-    query = f"INSERT INTO collections (name, geom) VALUES ('{name}', 'POINT({latitude} {longitude})');"
+    query = f"INSERT INTO collections (name, geom) VALUES ('{name}', 'POINT({longitude} {latitude})');"
     executeQuery(query)
     
 def insertImage():
     url = 'path:to:image'
     collection_id = 1
+    latitude = 00.22221
+    longitude = 11.231230
 
     # Query per l'inserimento di una nuova immagine
-    query = f"INSERT INTO images (url, collection_id) VALUES ('{url}', '{collection_id}');"
+    query = f"INSERT INTO images (url, geom, collection_id) VALUES ('{url}', 'POINT({longitude} {latitude})', '{collection_id}');"
     executeQuery(query)
 
 # Crea tabella delle collezioni
@@ -80,7 +82,13 @@ def createImageTable():
             id SERIAL PRIMARY KEY,
             url VARCHAR(255),
             timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            geom GEOMETRY(Point),
             collection_id INTEGER REFERENCES collections(id)
         );
         """
     executeQuery(query);
+
+createCollectionTable()
+createImageTable()
+insertCollection()
+insertImage()
