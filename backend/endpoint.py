dfd35ec2b6d39b3bc41b818a75efb2bc9972e9e8
mrpotrol <109791017+mrpotrol@users.noreply.github.com>
2023-09-04 18:41:54 +0200

    Compressione immagini

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
+from PIL import Image
 import base64
+import io
 import datetime
 import json
 import os
@@ -15,7 +17,6 @@ def getCollection():
     print(data)
     return sql.selectNCollections(data['longitudine'], data['latitudine'], data['n'])
 
-# Fornisce le N immagini più vicine alle coordinate fornite
 @app.route('/getImages', methods=['POST'])
 def getImages():
     print(request.data)
@@ -29,13 +30,23 @@ def getImages():
             image_data = f.read()
 
         del image['url']
-        # Codifica l'immagine in base64
-        image['base64image'] = base64.b64encode(image_data).decode('utf-8')
+        
+        # Comprimi l'immagine con Pillow
+        image_buffer = io.BytesIO(image_data)
+        img = Image.open(image_buffer)
+        img = img.convert("RGB")
+        output_buffer = io.BytesIO()
+        img.save(output_buffer, format="JPEG", quality=70)  # Modifica il valore della qualità secondo le tue esigenze
+        
+        # Codifica l'immagine compressa in base64
+        compressed_image_data = output_buffer.getvalue()
+        image['base64image'] = base64.b64encode(compressed_image_data).decode('utf-8')
 
     # Formatta la lista di dizionari in JSON
     data_json = json.dumps(images, indent=4)
 
     return data_json
+
 
 # Crea una nuova collezione
 @app.route('/newCollection', methods=['POST'])
@@ -50,7 +61,7 @@ def newCollection():
 def insertPhoto():
     data = request.get_json()
     imgdata = base64.b64decode(data['base64image']) 
-    dir_name = 'backend\images'
+    dir_name = 'backend/images'
     file_name = data['collectionId'] + '_' + datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S') + '.jpg'
 
     # Crea la directory se non esiste
