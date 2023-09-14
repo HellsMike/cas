6c52b67fc13bf5dfb15637ba42745f6833a1c2ef
Michele Bianco <bianco.michele@outlook.com>
2023-09-14 16:15:49 +0200

    Fix colorazione e gestione radio btn

--- frontend/public/map_display.js
+++ frontend/public/map_display.js
@@ -13,6 +13,8 @@ var southWest = L.latLng(-89.98155760646617, -180),
 mainMap.setMaxBounds(bounds);
 
 var geojson; // File GEOJSON
+var polygonLayerGroup = L.layerGroup(); // Gruppo layer per i poligoni
+var polygonLayerBorderGroup = L.layerGroup(); // Gruppo layer per i bordi dei poligoni
 
 var markers = []; // Array per tenere traccia dei marker
 var markerLayer; // Layer per i marker
@@ -28,18 +30,26 @@ for (var i = 0; i < markerRadios.length; i++) {
                 case 'none':
                     // Rimuove eventuali marker presenti
                     mainMap.removeLayer(markerLayer)
+                    markers = []
+
                     break;
                 case 'global':
                     // Aggiunge i marker globali
                     if (mainMap.hasLayer(markerLayer))
                         mainMap.removeLayer(markerLayer)
-                    addGlobalMarkers(); 
+                        markers = []
+
+                    addGlobalMarkers();
+
                     break;
                 case 'local':
                     // Aggiunge i marker all'interno del GEOJSON
                     if (mainMap.hasLayer(markerLayer))
                         mainMap.removeLayer(markerLayer)
+                        markers = []
+
                     addLocalMarkers();
+
                     break;
             }
         }
@@ -55,16 +65,48 @@ for (var i = 0; i < colorRadios.length; i++) {
         if (this.checked) {
             switch(this.value) {
                 case 'none':
-                    // Rimuove eventuali colorazioni presenti
+                    // Rimuove eventuali colorazioni presenti e ripristina solo i bordi
+                    mainMap.removeLayer(polygonLayerGroup)
+
+                    if (!mainMap.hasLayer(polygonLayerBorderGroup))
+                        polygonLayerBorderGroup.addTo(mainMap)
+
+                    polygonLayerGroup = L.layerGroup();
+
                     break;
                 case 'local':
                     // Aggiunge la colorazione all'interno del GEOJSON
+                    if (mainMap.hasLayer(polygonLayerGroup))
+                        mainMap.removeLayer(polygonLayerGroup)
+
+                    if (mainMap.hasLayer(polygonLayerBorderGroup))
+                        mainMap.removeLayer(polygonLayerBorderGroup)
+
+                    polygonLayerGroup = L.layerGroup();
+                    colorMap();
+
                     break;
                 case 'global_heatmap':
                     // Aggiunge la heatmap globale
+                    if (mainMap.hasLayer(polygonLayerGroup))
+                        mainMap.removeLayer(polygonLayerGroup)
+
+                    if (mainMap.hasLayer(polygonLayerBorderGroup))
+                        mainMap.removeLayer(polygonLayerBorderGroup)
+
+                    polygonLayerGroup = L.layerGroup();
+
                     break;
                 case 'local_heatmap':
                     // Aggiunge la heatmap all'interno del GEOJSON
+                    if (mainMap.hasLayer(polygonLayerGroup))
+                        mainMap.removeLayer(polygonLayerGroup)
+
+                    if (mainMap.hasLayer(polygonLayerBorderGroup))
+                        mainMap.removeLayer(polygonLayerBorderGroup)
+
+                    polygonLayerGroup = L.layerGroup();
+
                     break;
             }
         }
@@ -74,22 +116,22 @@ for (var i = 0; i < colorRadios.length; i++) {
 // Funzione per aggiungere marker globali
 function addGlobalMarkers() {
     fetch(backendEndpoint + '/getGlobalMarkers')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(item => {
-                // Crea un nuovo marker per ogni elemento in data
-                var marker = L.marker([item.latitudine, item.longitudine])
-                    .bindPopup(item.nome_collezione)
-                    .openPopup();
-    
-                markers.push(marker); // Aggiungi il marker all'array
-            });
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            // Crea un nuovo marker per ogni elemento in data
+            var marker = L.marker([item.latitudine, item.longitudine])
+                .bindPopup(item.nome_collezione)
+                .openPopup();
 
-            // Aggiungi i marker alla mappa principale
-            markerLayer = L.layerGroup(markers);
-            markerLayer.addTo(mainMap);   
-        })
-        .catch(error => console.error('Error:', error));
+            markers.push(marker); // Aggiungi il marker all'array
+        });
+
+        // Aggiungi i marker alla mappa principale
+        markerLayer = L.layerGroup(markers);
+        markerLayer.addTo(mainMap);   
+    })
+    .catch(error => console.error('Error:', error));
 }
 
 // Funzione per aggiungere marker locali
@@ -101,22 +143,22 @@ function addLocalMarkers() {
           },
           body: JSON.stringify(geojson),
     })
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(item => {
-                // Crea un nuovo marker per ogni elemento in data
-                var marker = L.marker([item.latitudine, item.longitudine])
-                    .bindPopup(item.nome_collezione)
-                    .openPopup();
-    
-                markers.push(marker); // Aggiungi il marker all'array
-            });
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            // Crea un nuovo marker per ogni elemento in data
+            var marker = L.marker([item.latitudine, item.longitudine])
+                .bindPopup(item.nome_collezione)
+                .openPopup();
 
-            // Aggiungi i marker alla mappa principale
-            markerLayer = L.layerGroup(markers);
-            markerLayer.addTo(mainMap);   
-        })
-        .catch(error => console.error('Error:', error));
+            markers.push(marker); // Aggiungi il marker all'array
+        });
+
+        // Aggiungi i marker alla mappa principale
+        markerLayer = L.layerGroup(markers);
+        markerLayer.addTo(mainMap);   
+    })
+    .catch(error => console.error('Error:', error));
 }
 
 // Funzione per gestire il caricamento del file GeoJSON
@@ -129,29 +171,43 @@ function handleFileSelect(event) {
             geojson = JSON.parse(e.target.result);
 
             if (geojson.type === "FeatureCollection" && geojson.features) {
-                // Mostra il contenitore dei poligoni e crea la mappa dei poligoni
-                const polygonsContainer = document.getElementById("map");
-                polygonsContainer.style.display = "block";
+                // Rimuove eventuali bordi e colorazioni legati ad altri geojson
+                if (mainMap.hasLayer(polygonLayerGroup))
+                mainMap.removeLayer(polygonLayerGroup)
+
+                if (mainMap.hasLayer(polygonLayerBorderGroup))
+                    mainMap.removeLayer(polygonLayerBorderGroup)
+
+                polygonLayerBorderGroup = L.layerGroup();
+
+                // Rimuove eventuali marker presenti sulla mappa
+                if (mainMap.hasLayer(markerLayer))
+                    mainMap.removeLayer(markerLayer)
+                    markers = []
 
                 geojson.features.forEach(function(feature) {
                     if (feature.geometry.type === "MultiPolygon") {
                         // Crea un layer con il poligono e il bordo senza colore
-                        var polygonLayer = L.geoJSON(feature, {
+                        var polygonLayerBorder = L.geoJSON(feature, {
                             style: {
                                 fillOpacity: 0, // Opacità del riempimento
                                 color: "black", // Colore del bordo
                                 weight: 1, // Spessore del bordo
                             },
-                    });
-
-                    polygonLayer.addTo(mainMap)
-                    
-                    // Rende cliccabili i radio button legati ai filtri spaziali
-                    document.getElementById('local_marker').disabled = false;   
-                    document.getElementById('local_color').disabled = false;
-                    document.getElementById('local_heatmap').disabled = false;
+                        });
+                        
+                        polygonLayerBorderGroup.addLayer(polygonLayerBorder);
                     }
                 });
+                
+                polygonLayerBorderGroup.addTo(mainMap)
+                // Imposta i radio button ai valori standard
+                document.getElementById('no_color').checked = true;
+                document.getElementById('no_marker').checked = true;
+                // Rende cliccabili i radio button legati ai filtri spaziali
+                document.getElementById('local_marker').disabled = false;   
+                document.getElementById('local_color').disabled = false;
+                document.getElementById('local_heatmap').disabled = false;
             } else {
                 console.error("Il file GeoJSON non contiene un MultiPolygon.");
             }
@@ -160,63 +216,70 @@ function handleFileSelect(event) {
     }
 }
 
-//funzione per verificare se un Marker è contenuto in un polygon
+// Verifica se un Marker è contenuto in un polygon
 function isMarkerInsidePolygon(marker, polygonFeature) {
     var polygon = L.geoJSON(polygonFeature);
     return polygon.getBounds().contains(marker.getLatLng());
 }
 
-/*
-    // Funzione per gestire il caricamento del file GeoJSON
-    function handleFileSelect(event) {
-        const file = event.target.files[0];
+// Genera la colorazione dei poligoni
+function colorMap() {
+    if (geojson.type === "FeatureCollection" && geojson.features) {
+        var colorMarkers = []; // Lista temporanea dei marker
+                
+        // Ottiene la lista di marker locali
+        fetch(backendEndpoint + '/getLocalMarkers', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(geojson),
+        })
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(item => {
+                // Crea un nuovo marker per ogni elemento in data
+                var marker = L.marker([item.latitudine, item.longitudine])
+                    .bindPopup(item.nome_collezione)
+                    .openPopup();
     
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = function(e) {
-                const geojson = JSON.parse(e.target.result);
-    
-                if (geojson.type === "FeatureCollection" && geojson.features) {
-                    // Mostra il contenitore dei poligoni e crea la mappa dei poligoni
-                    const polygonsContainer = document.getElementById("map");
-                    polygonsContainer.style.display = "block";
-    
-                    geojson.features.forEach(function(feature) {
-                        if (feature.geometry.type === "MultiPolygon") {
-                            var markerCount=0; // Contatore di marker nel singolo poligono
-                            // Calcola il conteggio dei marker all'interno del poligono
-                            cityMarkers.forEach(function(marker){
-                                if (isMarkerInsidePolygon(marker, feature)) {
-                                    markerCount++;
-                                }
-                            })
-                            // Imposta il colore in base al conteggio dei marker
-                            var fillColor = "red"; // Colore predefinito se nessun marker è presente
-                            if (markerCount > 0) {
-                            fillColor = markerCount < 2 ? "yellow" : "green";
-                            }
-                            // Crea un layer con il poligono e il colore appropriato
-                            var polygonLayer = L.geoJSON(feature, {
-                                style: {
-                                    fillColor: fillColor,
-                                    fillOpacity: 0.5, // Opacità del riempimento
-                                    color: "black", // Colore del bordo
-                                    weight: 2, // Spessore del bordo
-                                },
-                        });
-    
-                        polygonLayer.addTo(mainMap)
+                colorMarkers.push(marker); // Aggiunge il marker all'array
+            });
+            
+            // Mostra il contenitore dei poligoni e crea la mappa dei poligoni
+            geojson.features.forEach(function(feature) {
+                if (feature.geometry.type === "MultiPolygon") {
+                    var markerCount = 0; // Contatore di marker nel singolo poligono
+
+                    // Calcola il conteggio dei marker all'interno del poligono
+                    colorMarkers.forEach(function(marker){
+                        if (isMarkerInsidePolygon(marker, feature)) {
+                            markerCount++;
+                        }
+                    })
+
+                    // Imposta il colore in base al conteggio dei marker
+                    var fillColor = "red"; // Colore predefinito se nessun marker è presente
+                    
+                    if (markerCount > 0) 
+                        fillColor = markerCount < 2 ? "yellow" : "green";
+                    
+                    // Crea un layer con il poligono e il colore appropriato
+                    var polygonLayer = L.geoJSON(feature, {
+                        style: {
+                            fillColor: fillColor,
+                            fillOpacity: 0.4, // Opacità del riempimento
+                            color: "black", // Colore del bordo
+                            weight: 1, // Spessore del bordo
                         }
                     });
-    
-                    // Aggiungi i marker delle città alla mappa dei poligoni
-                    var cityLayer = L.layerGroup(cityMarkers);
-                    cityLayer.addTo(mainMap);
-                } else {
-                    console.error("Il file GeoJSON non contiene un MultiPolygon.");
+
+                    polygonLayerGroup.addLayer(polygonLayer);
                 }
-            };
-            reader.readAsText(file);
-        }
+            });
+
+            polygonLayerGroup.addTo(mainMap);
+        })
+        .catch(error => console.error('Error:', error));
     }
-    */
+}
