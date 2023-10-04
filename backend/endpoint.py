f07cafe65c8e526dda6a31852ea5a5eb8ca6139f
mrpotrol <109791017+mrpotrol@users.noreply.github.com>
2023-10-04 17:35:31 +0200

    Precisione timestamp aumentata

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -201,7 +201,7 @@ def insertPhoto():
     data = request.get_json()
     imgdata = base64.b64decode(data['base64image']) 
     dir_name = 'backend/images'
-    file_name = data['collectionId'] + '_' + datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S') + '.jpg'
+    file_name = data['collectionId'] + '_' + datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S-%f') + '.jpg'
 
     # Crea la directory se non esiste
     if not os.path.exists(dir_name):
